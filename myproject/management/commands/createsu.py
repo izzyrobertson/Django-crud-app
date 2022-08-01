182cfac1ed912b22aa1a59ee885b4d11d3c5698d
izzyrobertson <270112252@yoobeestudent.ac.nz>
2022-08-01 21:14:51 +1200

    Debugging createsu command

--- myproject/management/commands/createsu.py
+++ myproject/management/commands/createsu.py
@@ -1,8 +1,6 @@
-print("Inside createsu.py")
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
-print("What's happening?")
 
 class Command(BaseCommand):
 
@@ -12,6 +10,6 @@ class Command(BaseCommand):
 		# If it does not exist, then create a super-user with that username,
 		# Supply an email address and a password to it
 		# So, now we have an admin user to manage our AWS RDS database
-		if not User.objects.filter(username="awsadmin").exists()
-			print("What's happening over here?")
-			User.objects.create_superuser("awsadmin", "arthur.lewis420@gmail.com", "AWSAdmin123")
+		# if not User.objects.filter(username="awsadmin").exists()
+		# 	print("What's happening over here?")
+		# 	User.objects.create_superuser("awsadmin", "arthur.lewis420@gmail.com", "AWSAdmin123")
